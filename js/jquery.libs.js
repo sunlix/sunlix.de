112cf3ad6e724129847786e1849479a1aadcf99d
Sven Schüring <ssch@zenmotion.de>
2012-12-07 22:16:19 +0100

    - added support for shell syntax (jquery.snippet.js)

--- js/jquery.libs.js
+++ js/jquery.libs.js
@@ -3099,6 +3099,99 @@ sh_languages['url']=[[{'regex':/(?:<?)[A-Za-z0-9_\.\/\-_]+@[A-Za-z0-9_\.\/\-_]+(
 
 /* XML language (http://shjs.sourceforge.net/lang/sh_xml.min.js) */
 if(!this.sh_languages){this.sh_languages={}}sh_languages.xml=[[[/<\?xml/g,"sh_preproc",1,1],[/<!DOCTYPE/g,"sh_preproc",3,1],[/<!--/g,"sh_comment",4],[/<(?:\/)?[A-Za-z](?:[A-Za-z0-9_:.-]*)(?:\/)?>/g,"sh_keyword",-1],[/<(?:\/)?[A-Za-z](?:[A-Za-z0-9_:.-]*)/g,"sh_keyword",5,1],[/&(?:[A-Za-z0-9]+);/g,"sh_preproc",-1]],[[/\?>/g,"sh_preproc",-2],[/([^=" \t>]+)([ \t]*)(=?)/g,["sh_type","sh_normal","sh_symbol"],-1],[/"/g,"sh_string",2]],[[/\\(?:\\|")/g,null,-1],[/"/g,"sh_string",-2]],[[/>/g,"sh_preproc",-2],[/([^=" \t>]+)([ \t]*)(=?)/g,["sh_type","sh_normal","sh_symbol"],-1],[/"/g,"sh_string",2]],[[/-->/g,"sh_comment",-2],[/<!--/g,"sh_comment",4]],[[/(?:\/)?>/g,"sh_keyword",-2],[/([^=" \t>]+)([ \t]*)(=?)/g,["sh_type","sh_normal","sh_symbol"],-1],[/"/g,"sh_string",2]]];
+if (! this.sh_languages) {
+  this.sh_languages = {};
+}
+sh_languages['sh'] = [
+  [
+    [
+      /\b(?:import)\b/g,
+      'sh_preproc',
+      -1
+    ],
+    [
+      /\b[+-]?(?:(?:0x[A-Fa-f0-9]+)|(?:(?:[\d]*\.)?[\d]+(?:[eE][+-]?[\d]+)?))u?(?:(?:int(?:8|16|32|64))|L)?\b/g,
+      'sh_number',
+      -1
+    ],
+    [
+      /\\"|\\'/g,
+      'sh_normal',
+      -1
+    ],
+    [
+      /"/g,
+      'sh_string',
+      1
+    ],
+    [
+      /'/g,
+      'sh_string',
+      2
+    ],
+    [
+      /function[ \t]+(?:[A-Za-z]|_)[A-Za-z0-9_]*[ \t]*(?:\(\))?|(?:[A-Za-z]|_)[A-Za-z0-9_]*[ \t]*\(\)/g,
+      'sh_function',
+      -1
+    ],
+    [
+      /(?:[A-Za-z]*[-\/]+[A-Za-z]+)+/g,
+      'sh_normal',
+      -1
+    ],
+    [
+      /\b(?:alias|bg|bind|break|builtin|caller|case|command|compgen|complete|continue|declare|dirs|disown|do|done|elif|else|enable|esac|eval|exec|exit|export|false|fc|fg|fi|for|getopts|hash|help|history|if|in|jobs|let|local|logout|popd|printf|pushd|read|readonly|return|select|set|shift|shopt|source|suspend|test|then|times|trap|true|type|typeset|umask|unalias|unset|until|wait|while)\b/g,
+      'sh_keyword',
+      -1
+    ],
+    [
+      /(?:[A-Za-z]|_)[A-Za-z0-9_]*(?==)|\$\{(?:[^ \t]+)\}|\$\((?:[^ \t]+)\)|\$(?:[A-Za-z]|_)[A-Za-z0-9_]*|\$(?:[^ \t]{1})/g,
+      'sh_variable',
+      -1
+    ],
+    [
+      /~|!|%|\^|\*|\(|\)|\+|=|\[|\]|\\|:|;|,|\.|\/|\?|&|<|>|\||%%|(?:##){2}(?!#)/g,
+      'sh_symbol',
+      -1
+    ],
+    [
+      /#/g,
+      'sh_comment',
+      3
+    ]
+  ],
+  [
+    [
+      /"/g,
+      'sh_string',
+      -2
+    ],
+    [
+      /\\./g,
+      'sh_specialchar',
+      -1
+    ]
+  ],
+  [
+    [
+      /'/g,
+      'sh_string',
+      -2
+    ],
+    [
+      /\\./g,
+      'sh_specialchar',
+      -1
+    ]
+  ],
+  [
+    [
+      /$/g,
+      null,
+      -2
+    ]
+  ]
+];
 /*!
 jQuery Waypoints - v1.1.7
 Copyright (c) 2011-2012 Caleb Troughton

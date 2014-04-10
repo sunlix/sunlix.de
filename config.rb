e41533a5b2ff816af17d9432444081ad6f11efac
Sven Schüring <ssch@zenmotion.de>
2014-04-11 00:16:31 +0200

    updated YAML 4.1.2 and working directory

--- config.rb
+++ config.rb
@@ -1,11 +1,15 @@
+Encoding.default_external = 'utf-8'
+# Set project encoding
+encoding = 'utf-8'
+
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "js"
+http_path = '/'
+css_dir = 'assets/css'
+sass_dir = 'sass'
+images_dir = 'assets/img'
+javascripts_dir = 'assets/js'
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
